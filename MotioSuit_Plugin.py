e3ab8771c97db555988189f63f716ce95b4f6374
Arkmut <xavier.dussert@gmail.com>
2018-03-20 15:21:11 +0100

    ajout d'une liste contenant les ids des capteurs(adresse mac?), leurs nom dans l'armature, et le quaternion d'offset

--- MotioSuit_Plugin.py
+++ MotioSuit_Plugin.py
@@ -49,7 +49,16 @@ threadLock = None
 
 #---------------------------------------------------- Server-----------------------------------------------------       
             
-
+def findNameInDns(dns,id):
+    for item in dns:
+        if(item.id==id):
+            return item.name
+    return "notFound"
+def findOffSetInDns(dns,name):
+    for item in dns:
+        if(item.name==name):
+            return item.rot
+    return None
 def asyncReading(object,value,frame):
     #print("trigger Async: "+value)
     global area
@@ -61,7 +70,9 @@ def asyncReading(object,value,frame):
     angles = pileQuaternions[value]
     
     #apply rotation
-    pbase.rotation_quaternion = mathutils.Quaternion((angles[0],angles[1],angles[2],angles[3]))
+    offset=findOffSetInDns(bpy.types.Scene.dns,value)
+    newRot=mathutils.Quaternion((angles[0],angles[1],angles[2],angles[3]))
+    pbase.rotation_quaternion = newRot.rotation_difference(offset)
     global timeInit
     global interFrames
     global timePeriod
@@ -100,22 +111,58 @@ class myHandler(BaseHTTPRequestHandler):
         #print("receiving msg:" +data)
         #data deserialised (with json)
         sensorData=json.loads(data)
+        id=sensorData["sensor"]["id"]
         #angles is a matrix of size 1*4, containing a quaternion received
         angles=[sensorData["sensor"]["quaternion"]["w"],sensorData["sensor"]["quaternion"]["x"],sensorData["sensor"]["quaternion"]["y"],sensorData["sensor"]["quaternion"]["z"]]
         #check if the data is valid (and not cut by the network, or even broken before sending)
         isValid=sensorData["sensor"]["isDataValid"]
-        pileQuaternions["Tronc"]=angles
+        name=findNameInDns(bpy.types.Scene.dns,id)
+        pileQuaternions[name]=angles
         #TODO detect bone concerned by the data
         #example for one bone
         global threadLock
         with threadLock:
             #print("triggerUpdate")
-            bpy.data.objects["Armature"]["trigger"]="Tronc"        
+            bpy.data.objects["Armature"]["trigger"]=name        
         return 
 
 
 '''blender infrastructure for plugins'''
-#ui class
+#ui class-------------------------------------------------------------------------------------------------------------
+class DNSItem(bpy.types.PropertyGroup):
+    """ Group of properties representing an item in the list """
+
+    id =  bpy.props.IntProperty(
+           name="id",
+           description="sensor id",
+           default=-1)
+
+    name =  bpy.props.StringProperty(
+           name="name",
+           description="Bone name",
+           default="Tronc")
+    rot =  bpy.props.FloatVectorProperty(
+           name="rot",
+           description="quaternion rotation",
+           default=[0,0,0,0],
+           subtype ='QUATERNION',
+           size=4)
+           
+class DNS_UI_List(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+
+        # We could write some code to decide which icon to use here...
+        custom_icon = 'OBJECT_DATAMODE'
+
+        # Make sure your code supports all 3 layout types
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.label(item.name, icon = custom_icon)
+
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label("", icon = custom_icon)           
+
+            
 class VIEW3D_PT_MotioCapture(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
@@ -130,7 +177,22 @@ class VIEW3D_PT_MotioCapture(bpy.types.Panel):
         #adding buttons to launch and stop the recording
         TheCol.operator("object.motiocapture", text = "MotionCapture")
         TheCol.operator("object.motiocapturestop", text = "MotionCaptureStop")
+        TheCol.operator("object.updatebonelist", text = "UpdateBoneList")
+        layout = self.layout
+        scene = context.scene
+        row = layout.row()
+        row.template_list("DNS_UI_List", "DNS", scene, "dns", scene, "dns_index" )
+
+        row = layout.row()
+        
             
+        if scene.dns_index >= 0 and len(scene.dns) > 0:
+            item = scene.dns[scene.dns_index]
+
+            row = layout.row()
+            row.prop(item, "id")
+            row.prop(item, "name")
+            row.prop(item, "rot")            
         
 
 #operator to start the server (and the recording)
@@ -176,6 +238,25 @@ class MotioCaptureStop(bpy.types.Operator):
         return {'FINISHED'}
     #end invoke
 
+#set the bone list
+class UpdateBoneList(bpy.types.Operator):
+    bl_idname = "object.updatebonelist"
+    bl_label = "UpdateBoneList"
+    bl_options = {'REGISTER', 'UNDO', 'PRESET'}
+   
+    def execute(self, context):
+        print('update list')
+        scn = bpy.context.scene
+        scn.dns.clear()
+        i=0
+        for b in bpy.data.objects["Armature"].pose.bones:
+            item=scn.dns.add()
+            item.id=i
+            item.name=b.name
+            i+=1
+        return {'FINISHED'}
+    #end invoke
+
 #class wrapping the server in a thread
 class ThreadListener(Thread):
     def __init__(self):
@@ -207,6 +288,8 @@ class ThreadListener(Thread):
 def register():
     bpy.utils.register_module(__name__)
     bpy.app.handlers.scene_update_pre.append(my_handler) 
+    bpy.types.Scene.dns =  bpy.props.CollectionProperty(type = DNSItem)
+    bpy.types.Scene.dns_index =  bpy.props.IntProperty(name = "Index for DNS", default = 0)
     print("loading plugin")
     
     
